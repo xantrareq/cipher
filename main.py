506e27a0658eafb9bae4cbe7c0ede184f1b354a3
heh <xantrareq@gmail.com>
2022-05-30 00:26:40 +0300

    добавил функцию шифрования

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def cipher(key, text):
+    size=len(key)
+    n=len(key)
+    block =' '
+    code= ' '
+    for i in range(0,n,size):
+        block=[text[i+j] for j in range(size)]
+        for j in range(size):
+            code +=block[key.index(j)]
+    return code
+
 print("Привет")
 print("Я умею шифровать сообщения")
 print("Привет")
@@ -9,3 +20,4 @@ if vib == 1:
     print("Вы выбрали действие 1,круто)")
 else:
     print("Действие два? Круто)")
+    print("zzzz")
