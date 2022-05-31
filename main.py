afa3f332562c134daa5d75e2348efdb0f45948f8
heh <xantrareq@gmail.com>
2022-05-31 23:50:38 +0300

    поправочки

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-def cipher(key, text):
+def choice():
+    txt=input("Введите сообщение: ")
+    print("Введите ключ шифрования, вводя цифры через пробел")
+    print("Например, 3 1 0 2")
+    a=input("Введите ключ")
+    ak=a.split()
+    key=[]
+    for i in ak:
+        key.append(int(i))
+    print("Выберите тип шифрования: ")
+    print("1 - посимвольное")
+    print("2 - группы")
+    print("3 - слов \n")
+    kek = int()
+    if kek==1:
+        simv(key,txt)
+def simv(key,txt):
     size=len(key)
-    n=len(key)
-    block =' '
-    code= ' '
-    for i in range(0,n,size):
-        block=[text[i+j] for j in range(size)]
-        for j in range(size):
-            code +=block[key.index(j)]
-    return code
-def simv(key,text):
-    size=len(key)
-    n=len(text)
+    n=len(txt)
     if n%size !=0:
         for i in range (size-(n%size)):
-            text+=str(cipher(key,text))
-        print(cipher(key,text))
+            txt+=str(cipher(key,txt))
+        print(cipher(key,txt))
+def cipher(key, txt):
+    size=len(key)
+    n=len(key)
+    zed =' '
+    cyp= ' '
+    for i in range(0,n,size):
+        zed=[txt[i+j] for j in range(size)]
+        for j in range(size):
+            cyp +=zed[key.index(j)]
+    return cyp
 
 print("Привет")
 print("Я умею шифровать сообщения")
-print("Привет")
 print("Выберите действие:")
 print("1 - зашифровать")
 print("2 - расшифровать")
@@ -27,4 +42,4 @@ if vib == 1:
     print("Вы выбрали действие 1,круто)")
 else:
     print("Действие два? Круто)")
-    print("zzzz")
+
