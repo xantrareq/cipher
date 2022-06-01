841446a555ffc68975db4d901c893b7fc71e3e13
heh <xantrareq@gmail.com>
2022-06-01 18:12:59 +0300

    Часть дешифратора и поправки

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def cipher(key, txt):
     size=len(key)
     n=len(key)
     zed = ' '
-    cipher = ' '
+    cyp = ' '
     for i in range(0,n,size):
         zed=[txt[i+j] for j in range(size)]
         for j in range(size):
-            cipher += zed[key.index(j)]
-    return cipher
+            cyp += zed[key.index(j)]
+    return cyp
 
 def symbol(key, txt):
     size=len(key)
@@ -17,16 +17,31 @@ def symbol(key, txt):
             txt += str("/0")
     print(cipher(key, txt))
 def groups(key, txt):
-    blocksize = len(key)
-    poskolko = int(input("По сколько символов нужно группировать? "))
-    text = [txt[i:i + poskolko] for i in range(0, len(txt), poskolko)]
-    if len(text[-1]) != poskolko:
-        for i in range(poskolko - (len(text[-1]) % poskolko)):
+    size = len(key)
+    kolvo = int(input("По сколько символов нужно группировать? "))
+    text = [txt[i:i + kolvo] for i in range(0, len(txt), kolvo)]
+    if len(text[-1]) != kolvo:
+        for i in range(kolvo - (len(text[-1]) % kolvo)):
             text[-1] += str("/0")
-    if len(text) != blocksize:
-        for i in range(blocksize - (len(text) % blocksize)):
-            text.append("/0" * poskolko)
+    if len(text) != size:
+        for i in range(size - (len(text) % size)):
+            text.append("/0" * kolvo)
     print(cipher(key, text))
+def word(key, txt):
+    size = len(key)
+    text = txt.split(" ")
+    if len(text) != size:
+        for i in range(size - (len(text) % size)):
+            text.append("/0" * 5)
+    k = len(text)
+    block = ''
+    cyp = ''
+    for i in range(0, k, size):
+        block = [text[i + j] for j in range(size)]
+        for j in range(size):
+            cyp += block[key.index(j)]
+            cyp += " "
+    print(cyp)
 
 def choice():
     txt = input("Введите сообщение: ")
@@ -46,17 +61,35 @@ def choice():
         symbol(key, txt)
     if kek == 2:
         groups(key, txt)
+    if kek == 3:
+        word(key, txt)
+def decip(key, txt):
+    size = len(key)
+    n = len(txt)
+    block = ''
+    cyp = ''
+    for i in range(0, n, size):
+        block = [txt[i + j] for j in range(size)]
+        for j in range(size):
+            cyp += block[key.index(j)]
+    cyp = cyp.replace("/0", "")
+    return cyp
+def desymbol(key, txt):
+    for i in range(len(key) // 2):
+        key[i], key[-i - 1] = key[-i - 1], key[i]
+    print(decip(key, txt))
 def beggin():
     print("Привет")
     print("Я умею шифровать сообщения")
     print("Выберите действие:")
     print("1 - зашифровать")
     print("2 - расшифровать")
-    vib = int(input())
+    vib = int(input("Ваш выбор: "))
     if vib == 1:
         choice()
     else:
-        print("Действие два? Круто)")
+        print("Ладно")
+
 while(True):
     beggin()
     txt=0
