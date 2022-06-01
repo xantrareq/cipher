1844e911b34d1c638c0e909c2b48b01263824d15
heh <xantrareq@gmail.com>
2022-06-01 13:45:16 +0300

    Поправки рабочего неработающих функций

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
-def cipher(key, txt0):
+def cipher(key, txt):
     size=len(key)
     n=len(key)
-    zed =' '
-    cyp= ' '
+    zed = ' '
+    cipher = ' '
     for i in range(0,n,size):
-        zed=[txt0[i+j] for j in range(size)]
+        zed=[txt[i+j] for j in range(size)]
         for j in range(size):
-            cyp +=zed[key.index(j)]
-    print (cyp)
-    beggin()
+            cipher += zed[key.index(j)]
+    return cipher
+
+def symbol(key, txt):
+    size=len(key)
+    n=len(txt)
+    if n % size != 0:
+        for i in range(size-(n%size)):
+            txt += str("/0")
+    print(cipher(key, txt))
+def groups(key, txt):
+    blocksize = len(key)
+    poskolko = int(input("По сколько символов нужно группировать? "))
+    text = [txt[i:i + poskolko] for i in range(0, len(txt), poskolko)]
+    if len(text[-1]) != poskolko:
+        for i in range(poskolko - (len(text[-1]) % poskolko)):
+            text[-1] += str("/0")
+    if len(text) != blocksize:
+        for i in range(blocksize - (len(text) % blocksize)):
+            text.append("/0" * poskolko)
+    print(cipher(key, text))
+
 def choice():
-    txt=input("Введите сообщение: ")
+    txt = input("Введите сообщение: ")
     print("Введите ключ шифрования, вводя цифры через пробел")
     print("Например, 3 1 0 2")
-    a=input("Введите ключ")
-    ak=a.split()
-    key=[]
-    for i in ak:
-        key.append(int(i))
+    k = input("Введите ключ")
+    ke = k.split()
+    key = []
+    for e in ke:
+        key.append(int(e))
     print("Выберите тип шифрования: ")
     print("1 - посимвольное")
     print("2 - группы")
     print("3 - слов \n")
-    kek = int()
-    if kek==1:
-        symbol(key,txt)
-def symbol(key,txt):
-    size=len(key)
-    n=len(txt)
-    if n%size !=0:
-        for i in range (size-(n%size)):
-            txt+=str(cipher(key,txt))
-        print(cipher(key,txt))
-def groups(key,txt):
-    size=len(key)
-    kolvo=int(input("Введите количество символов в группе: "))
-    txt1=[txt[i:i+kolvo] for i in range(0,len(txt),kolvo)]
-    if len(txt1[-1]) !=kolvo:
-        for i in range(kolvo-(len(txt1[-1])%kolvo)):
-            txt1[-1]+=str("/0")
-    if len(txt)!=size:
-        for i in range(size-(len(txt1)%size)):
-            txt.append("/0"*kolvo)
-    print(cipher(key,txt))
+    kek = int(input())
+    if kek == 1:
+        symbol(key, txt)
+    if kek == 2:
+        groups(key, txt)
 def beggin():
     print("Привет")
     print("Я умею шифровать сообщения")
@@ -51,9 +54,9 @@ def beggin():
     print("2 - расшифровать")
     vib = int(input())
     if vib == 1:
-        print("Вы выбрали действие 1,круто)")
+        choice()
     else:
         print("Действие два? Круто)")
-
-beggin()
-
+while(True):
+    beggin()
+    txt=0
